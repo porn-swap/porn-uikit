072e457a489ffcc2433afd6875b2bb0e10638e63
Literally Collective <bluedragon.cstar@outlook.com>
2021-05-17 03:22:45 -0300

    Feat: update css

--- src/widgets/Menu/components/Logo.tsx
+++ src/widgets/Menu/components/Logo.tsx
@@ -22,7 +22,7 @@ const StyledLink = styled(Link)`
   display: flex;
   align-items: center;
   .mobile-icon {
-    width: 32px;
+    width: 50px;
     ${({ theme }) => theme.mediaQueries.nav} {
       display: none;
     }
